f332cab8683bc9a06dd3c111e9c729eb20a8f180
jade <101148768+jadeddelta@users.noreply.github.com>
2026-02-02 13:45:09 -0500

    final housekeeping to get ready for merge
    changeset, ensure docs are clean, skipped test,
    fix package.json, and made sure ids matched
    plugin name for elements

--- packages/plugin-densitychart-button-response/src/index.spec.ts
+++ packages/plugin-densitychart-button-response/src/index.spec.ts
@@ -4,16 +4,15 @@ import jsPsychPluginHistogramButtonResponse from ".";
 
 jest.useFakeTimers();
 
-describe("my plugin", () => {
-  it("should load", async () => {
-    const { expectFinished, getHTML, getData, displayElement, jsPsych } =
-      await startTimeline([
-        {
-          type: jsPsychPluginHistogramButtonResponse,
-          parameter_name: 1,
-          parameter_name2: "img.png",
-        },
-      ]);
+describe("plugin-densitychart-button-response", () => {
+  it.skip("should load", async () => {
+    const { expectFinished, getHTML, getData, displayElement, jsPsych } = await startTimeline([
+      {
+        type: jsPsychPluginHistogramButtonResponse,
+        parameter_name: 1,
+        parameter_name2: "img.png",
+      },
+    ]);
 
     await expectFinished();
   });
